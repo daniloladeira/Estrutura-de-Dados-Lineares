5b310f5b8c94032e00e51a8d24b365ce66fa2745
daniloladeira <daniloladeira20@gmail.com>
2024-09-30 12:14:26 -0300

    CircleArea

---
+++ src/exercises/CircleArea.java
@@ -0,0 +1,12 @@
+package exercises;
+
+public class CircleArea {
+    public static void main(String[] args) {
+        double radius = 3.4;
+        final double pi = 3.14159;
+
+        double area = pi * radius * radius;
+
+        System.out.println(area);
+    }
+}
