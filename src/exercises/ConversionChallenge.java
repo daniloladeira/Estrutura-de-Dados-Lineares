df667b77b22a4b2a9652cbd87136e76841660213
daniloladeira <daniloladeira20@gmail.com>
2024-10-07 20:42:42 -0300

    Conversion Challenge

--- src/exercises/ConversionChallenge.java
+++ src/exercises/ConversionChallenge.java
@@ -1,4 +1,31 @@
+//a partir do scanner pegar 3 strings usando nextline, cada string recebe 3 salarios de um funcionario e calcula a media
+// porem o usuario pode separar as casas decimais tanto com "," quanto com "." precisa converter para numerico e mostrar a media
+
 package exercises;
 
+import java.util.Scanner;
+
 public class ConversionChallenge {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("1° SALARY: ");
+        String salary1 = salary.nextLine().replace(",", ".");
+
+        System.out.println("2° SALARY: ");
+        String salary2 = salary.nextLine().replace(",", ".");
+
+        System.out.println("3° SALARY: ");
+        String salary3 = salary.nextLine().replace(",", ".");
+
+        double s1 = Double.parseDouble(salary1);
+        double s2 = Double.parseDouble(salary2);
+        double s3 = Double.parseDouble(salary3);
+
+        double salaryAverage = (s1 + s2 + s3) / 3;
+
+        input.close();
+        System.out.printf("This worker salary average is: %.2f\n", salaryAverage);
+    }
 }
