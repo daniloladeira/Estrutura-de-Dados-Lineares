ab65988ae4d9323adc51366228a09fb5b701a3a8
daniloladeira <daniloladeira20@gmail.com>
2024-10-07 18:47:05 -0300

    Number -> String

---
+++ src/exercises/NumberStringConversion.java
@@ -0,0 +1,12 @@
+package exercises;
+
+public class NumberStringConversion {
+
+    public static void main(String[] args) {
+        Integer n1 = 10000;
+        System.out.println(n1.toString());
+
+        int n2 = 100000;
+        System.out.println(Integer.toString(n2));
+    }
+}
