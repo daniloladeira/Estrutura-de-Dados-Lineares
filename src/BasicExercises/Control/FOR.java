e7814bd48d90b71d2bdb4954e9cd41f4f5782837
daniloladeira <daniloladeira20@gmail.com>
2024-10-19 21:47:42 -0300

    For loop

---
+++ src/BasicExercises/Control/FOR.java
@@ -0,0 +1,9 @@
+package BasicExercises.Control;
+
+public class FOR {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 10; i++) {
+            System.out.printf("i = %d\n", i);
+        }
+    }
+}
